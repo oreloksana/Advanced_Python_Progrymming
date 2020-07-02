7af6296bb6196ecc8980f1c56e73d71bca8be1fb
oreloksana <40369026+oreloksana@users.noreply.github.com>
2020-07-02 12:47:13 +0200

    :bar_chart:

--- animation.py
+++ animation.py
@@ -1,29 +1,33 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+
 class Particle:
     def __init__(self, x, y, ang_speed):
         self.x = x
         self.y = y
         self.ang_speed = ang_speed
 
+
 class ParticleSimulator:
     def __init__(self, particles):
         self.particles = particles
+
     def evolve(self, dt):
         timestep = 0.00001
-        nsteps = int(dt/timestep)
+        nsteps = int(dt / timestep)
         for i in range(nsteps):
             for p in self.particles:
-
-                norm = (p.x**2 + p.y**2)**0.5
-                v_x = (-p.y)/norm
-                v_y = p.x/norm
+                #calculate the direction
+                norm = (p.x ** 2 + p.y ** 2) ** 0.5
+                v_x = (-p.y) / norm
+                v_y = p.x / norm
 
                 d_x = timestep * p.ang_speed * v_x
                 d_y = timestep * p.ang_speed * v_y
                 p.x += d_x
                 p.y += d_y
+
 
 def visualize(simulator):
     X = [p.x for p in simulator.particles]
@@ -32,9 +36,9 @@ def visualize(simulator):
     ax = plt.subplot(111, aspect='equal')
     line, = ax.plot(X, Y, 'ro')
 
+
     plt.xlim(-1, 1)
     plt.ylim(-1, 1)
-
 
     def init():
         line.set_data([], [])
@@ -50,10 +54,12 @@ def visualize(simulator):
     anim = animation.FuncAnimation(fig, animate, init_func=init, blit=False)
     plt.show()
 
+
 def test_visualize():
-    particles = [Particle( 0.3, 0.5, +1), Particle( 0.0, -0.5, -1)]
+    particles = [Particle(0.3, 0.5, +1), Particle(0.0, -0.5, -1)]
     simulator = ParticleSimulator(particles)
     visualize(simulator)
+
 
 if __name__ == '__main__':
     test_visualize()
